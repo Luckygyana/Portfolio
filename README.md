5d5f9bc9178c09eb9188e6c5241326f2aff9be52
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2021-03-07 16:33:21 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+This project is my Portfolio made with React running at (http://Luckygyana.github.io/Portfolio).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
+import { DATA } from "./Components/Data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header data={DATA.main} />
+        <About data={DATA.main} />
+        <Resume data={DATA.resume} />
+        <Portfolio data={DATA.portfolio} />
+        <Contact data={DATA.main} />
+        <Footer data={DATA.main} />
+      </div>
+    );
+  }
 }
 
 export default App;
